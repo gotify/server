bbb82bd2b0cc638401f02dde40b6cd1462294fca
Jannis Mattheis <contact@jmattheis.de>
2018-02-10 14:22:44 +0100

    Add user api

--- model/user.go
+++ model/user.go
@@ -3,7 +3,7 @@ package model
 // The User holds information about the credentials of a user and its application and client tokens.
 type User struct {
 	ID           uint `gorm:"primary_key;unique_index;AUTO_INCREMENT"`
-	Name         string
+	Name         string `gorm:"unique_index"`
 	Pass         []byte
 	Admin        bool
 	Applications []Application
