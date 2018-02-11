082b054f4c479e0d8a4829b337fdb7a826baac8d
Jannis Mattheis <contact@jmattheis.de>
2018-02-11 10:54:15 +0100

    Improve build time
    The bcrypt hashing is really slow with enabled race condition checker,
    therefore we exlcude it.

--- all-test-with-coverage.sh
+++ all-test-with-coverage.sh
@@ -4,9 +4,10 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    go test -v -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
+go test -v -race ./...
