a60c2f2d2f1f129c4c4350a612ed404b864406c6
Jannis Mattheis <contact@jmattheis.de>
2018-02-20 17:41:56 +0100

    Add some logging & version info building

--- app.go
+++ app.go
@@ -4,14 +4,30 @@ import (
 	"math/rand"
 	"time"
 
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gotify/server/config"
 	"github.com/gotify/server/database"
+	"github.com/gotify/server/model"
 	"github.com/gotify/server/router"
 	"github.com/gotify/server/runner"
 )
 
+var (
+	// Version the version of Gotify.
+	Version   = "unknown"
+	// Commit the git commit hash of this version.
+	Commit    = "unknown"
+	// BuildDate the date on which this binary was build.
+	BuildDate = "unknown"
+	// Branch the git branch of this version.
+	Branch    = "unknown"
+	vInfo     = &model.VersionInfo{Version: Version, Commit: Commit, BuildDate: BuildDate, Branch: Branch}
+)
+
 func main() {
+	fmt.Println("Starting Gotify version", Version+"@"+BuildDate)
 	rand.Seed(time.Now().UnixNano())
 	conf := config.Get()
 	db, err := database.New(conf.Database.Dialect, conf.Database.Connection, conf.DefaultUser.Name, conf.DefaultUser.Pass)
@@ -21,7 +37,7 @@ func main() {
 	defer db.Close()
 
 	gin.SetMode(gin.ReleaseMode)
-	engine, closeable := router.Create(db)
+	engine, closeable := router.Create(db, vInfo)
 	defer closeable()
 
 	runner.Run(engine, conf)
