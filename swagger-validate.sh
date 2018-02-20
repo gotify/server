71d787b72100e64c947a25cb880f1a7fe6c303ee
Jannis Mattheis <contact@jmattheis.de>
2018-02-20 19:06:44 +0100

    Add debug output for swagger validate

--- swagger-validate.sh
+++ swagger-validate.sh
@@ -2,7 +2,9 @@
 
 swagger generate spec --scan-models -o docs/spec.json
 (cd docs && packr && git add .)
-if [[ `git status --porcelain | grep docs` ]]; then
+if [[ `git diff docs` ]]; then
+    git status --porcelain | grep docs
+    git diff docs
     echo Swagger or the Packr file is not up-to-date
     exit 1
 fi
