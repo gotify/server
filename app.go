a22ace4f7d6d4e0dea8b1b130078573c1404457e
Jannis Mattheis <contact@jmattheis.de>
2018-02-21 19:39:46 +0100

    Move vInfo (in var its not initialized yet)

--- app.go
+++ app.go
@@ -16,18 +16,19 @@ import (
 
 var (
 	// Version the version of Gotify.
-	Version   = "unknown"
+	Version = "unknown"
 	// Commit the git commit hash of this version.
-	Commit    = "unknown"
+	Commit = "unknown"
 	// BuildDate the date on which this binary was build.
 	BuildDate = "unknown"
 	// Branch the git branch of this version.
-	Branch    = "unknown"
-	vInfo     = &model.VersionInfo{Version: Version, Commit: Commit, BuildDate: BuildDate, Branch: Branch}
+	Branch = "unknown"
 )
 
 func main() {
-	fmt.Println("Starting Gotify version", Version+"@"+BuildDate)
+	vInfo := &model.VersionInfo{Version: Version, Commit: Commit, BuildDate: BuildDate, Branch: Branch}
+
+	fmt.Println("Starting Gotify version", vInfo.Version+"@"+BuildDate)
 	rand.Seed(time.Now().UnixNano())
 	conf := config.Get()
 	db, err := database.New(conf.Database.Dialect, conf.Database.Connection, conf.DefaultUser.Name, conf.DefaultUser.Pass)
