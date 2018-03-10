daec47f0c5cedd356bf8542bf88c046d9b9e5eaa
Jannis Mattheis <contact@jmattheis.de>
2018-03-10 21:18:12 +0100

    Add ID to model.Client & model.Application

--- model/client.go
+++ model/client.go
@@ -6,16 +6,22 @@ package model
 //
 // swagger:model Client
 type Client struct {
-	// The client id. Can be used as `clientToken`. See Authentication.
+	// The client id.
+	//
+	// read only: true
+	// required: true
+	// example: 5
+	ID uint `gorm:"primary_key;unique_index;AUTO_INCREMENT" json:"id"`
+	// The client token. Can be used as `clientToken`. See Authentication.
 	//
 	// read only: true
 	// required: true
 	// example: CWH0wZ5r0Mbac.r
-	ID     string `gorm:"primary_key;unique_index" json:"id"`
+	Token  string `gorm:"unique_index" json:"token"`
 	UserID uint   `gorm:"index" json:"-"`
 	// The client name. This is how the client should be displayed to the user.
 	//
 	// required: true
 	// example: Android Phone
-	Name   string `form:"name" query:"name" json:"name" binding:"required"`
+	Name string `form:"name" query:"name" json:"name" binding:"required"`
 }
