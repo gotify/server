8673762b82619f74c70620673e39d87f693203d9
Jannis Mattheis <contact@jmattheis.de>
2018-03-10 11:24:44 +0100

    Add packr travis-ci build step and remove packr file

--- docs/swagger_test.go
+++ docs/swagger_test.go
@@ -1,11 +1,11 @@
 package docs
 
 import (
+	"net/http/httptest"
 	"testing"
+
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"net/http/httptest"
-	"io/ioutil"
 )
 
 func TestServe(t *testing.T) {
@@ -17,13 +17,6 @@ func TestServe(t *testing.T) {
 
 	Serve(ctx)
 
-	actualFileContent := getActualSpecFileContent(t)
-	packrFileContent := recorder.Body.String()
-	assert.JSONEq(t, packrFileContent, actualFileContent, "packr and spec file are out of sync")
-}
-
-func getActualSpecFileContent(t *testing.T) string {
-	bytes, err := ioutil.ReadFile("spec.json")
-	assert.Nil(t, err)
-	return string(bytes)
+	content := recorder.Body.String()
+	assert.NotEmpty(t, content)
 }
