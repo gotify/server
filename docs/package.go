6c52d062476cd11d0c72bed962472e6e33aa78a1
Jannis Mattheis <contact@jmattheis.de>
2018-03-11 17:19:03 +0100

    Increase API version

--- docs/package.go
+++ docs/package.go
@@ -16,7 +16,7 @@
 //
 //     Schemes: http, https
 //     Host: localhost
-//     Version: 1.0.1
+//     Version: 1.0.2
 //     License: MIT https://github.com/gotify/server/blob/master/LICENSE
 //
 //     Consumes:
