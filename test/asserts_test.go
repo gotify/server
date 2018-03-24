d452b8b8aeb0b2ef8c454b31dca1527871e10895
Jannis Mattheis <contact@jmattheis.de>
2018-03-24 16:39:57 +0100

    Add asserts test util
    For comparing the body of a request with an instance.

---
+++ test/asserts_test.go
@@ -0,0 +1,42 @@
+package test_test
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gotify/server/test"
+	"github.com/stretchr/testify/assert"
+)
+
+type obj struct {
+	Test string
+	ID   int
+}
+
+type fakeTesting struct {
+	hasErrors bool
+}
+
+func (t *fakeTesting) Errorf(format string, args ...interface{}) {
+	t.hasErrors = true
+}
+
+func Test_BodyEquals(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	recorder.WriteString(`{"ID": 2, "Test": "asd"}`)
+
+	fakeTesting := &fakeTesting{}
+
+	test.BodyEquals(fakeTesting, &obj{ID: 2, Test: "asd"}, recorder)
+	assert.False(t, fakeTesting.hasErrors)
+}
+
+func Test_BodyEquals_failing(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	recorder.WriteString(`{"ID": 3, "Test": "asd"}`)
+
+	fakeTesting := &fakeTesting{}
+
+	test.BodyEquals(fakeTesting, &obj{ID: 2, Test: "asd"}, recorder)
+	assert.True(t, fakeTesting.hasErrors)
+}
