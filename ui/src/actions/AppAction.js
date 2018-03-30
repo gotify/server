857360b53ca9500f95b1dd89cc472b75079ed3dd
Jannis Mattheis <contact@jmattheis.de>
2018-03-30 19:33:59 +0200

    Add upload image action

--- ui/src/actions/AppAction.js
+++ ui/src/actions/AppAction.js
@@ -18,10 +18,22 @@ export function deleteApp(id) {
 }
 
 /**
- * Create an application
+ * Create an application.
  * @param {string} name the application name
  * @param {string} description the description of the application.
  */
 export function createApp(name, description) {
     axios.post(config.get('url') + 'application', {name, description}).then(fetchApps);
+}
+
+/**
+ * Upload an image for an application.
+ * @param {int} id the application id
+ * @param {Blob} file the description of the application.
+ */
+export function uploadImage(id, file) {
+    const formData = new FormData();
+    formData.append('file', file);
+    axios.post(config.get('url') + 'application/' + id + '/image', formData,
+        {headers: {'content-type': 'multipart/form-data'}}).then(fetchApps);
 }
