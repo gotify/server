f69eefd6ecefd18b167f3398d7c6326caf5c3a0e
Jannis Mattheis <contact@jmattheis.de>
2018-03-31 18:53:15 +0200

    Add snack action

--- ui/src/actions/GlobalAction.js
+++ ui/src/actions/GlobalAction.js
@@ -2,6 +2,7 @@ import * as AppAction from './AppAction';
 import * as UserAction from './UserAction';
 import * as MessageAction from './MessageAction';
 import * as ClientAction from './ClientAction';
+import dispatcher from '../stores/dispatcher';
 
 /** Calls all actions to initialize the state. */
 export function initialLoad() {
@@ -13,3 +14,6 @@ export function initialLoad() {
     UserAction.fetchUsers();
 }
 
+export function snack(message) {
+    dispatcher.dispatch({type: 'SNACK', payload: message});
+}
