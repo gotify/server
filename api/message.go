18040ea19922a982c1a79dc3395d6a018040ed3a
Jannis Mattheis <contact@jmattheis.de>
2018-04-08 14:32:47 +0200

    [#34] Adjust message api to be paged

--- api/message.go
+++ api/message.go
@@ -4,16 +4,20 @@ import (
 	"errors"
 	"time"
 
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/gotify/location"
 	"github.com/gotify/server/auth"
 	"github.com/gotify/server/model"
 )
 
 // The MessageDatabase interface for encapsulating database access.
 type MessageDatabase interface {
-	GetMessagesByApplication(id uint) []*model.Message
+	GetMessagesByApplicationSince(appID uint, limit int, since uint) []*model.Message
 	GetApplicationByID(id uint) *model.Application
-	GetMessagesByUser(userID uint) []*model.Message
+	GetMessagesByUserSince(userID uint, limit int, since uint) []*model.Message
 	DeleteMessageByID(id uint) error
 	GetMessageByID(id uint) *model.Message
 	DeleteMessagesByUser(userID uint) error
@@ -33,22 +37,61 @@ type MessageAPI struct {
 	Notifier Notifier
 }
 
+type pagingParams struct {
+	Limit int  `form:"limit" binding:"min=1,max=200"`
+	Since uint `form:"since" binding:"min=0"`
+}
+
 // GetMessages returns all messages from a user.
 func (a *MessageAPI) GetMessages(ctx *gin.Context) {
 	userID := auth.GetUserID(ctx)
-	messages := a.DB.GetMessagesByUser(userID)
-	ctx.JSON(200, messages)
+	withPaging(ctx, func(params *pagingParams) {
+		// the +1 is used to check if there are more messages and will be removed on buildWithPaging
+		messages := a.DB.GetMessagesByUserSince(userID, params.Limit+1, params.Since)
+		ctx.JSON(200, buildWithPaging(ctx, params, messages))
+	})
+}
+
+func buildWithPaging(ctx *gin.Context, paging *pagingParams, messages []*model.Message) *model.PagedMessages {
+	next := ""
+	since := uint(0)
+	useMessages := messages
+	if len(messages) > paging.Limit {
+		useMessages = messages[:len(messages)-1]
+		since = useMessages[len(useMessages)-1].ID
+		url := location.Get(ctx)
+		url.Path = ctx.Request.URL.Path
+		query := url.Query()
+		query.Add("limit", strconv.Itoa(paging.Limit))
+		query.Add("since", strconv.FormatUint(uint64(since), 10))
+		url.RawQuery = query.Encode()
+		next = url.String()
+	}
+	return &model.PagedMessages{
+		Paging:   model.Paging{Size: len(useMessages), Limit: paging.Limit, Next: next, Since: since},
+		Messages: useMessages,
+	}
+}
+
+func withPaging(ctx *gin.Context, f func(pagingParams *pagingParams)) {
+	params := &pagingParams{Limit: 100}
+	if err := ctx.MustBindWith(params, binding.Query); err == nil {
+		f(params)
+	}
 }
 
 // GetMessagesWithApplication returns all messages from a specific application.
 func (a *MessageAPI) GetMessagesWithApplication(ctx *gin.Context) {
 	withID(ctx, "id", func(id uint) {
-		if app := a.DB.GetApplicationByID(id); app != nil && app.UserID == auth.GetUserID(ctx) {
-			messages := a.DB.GetMessagesByApplication(id)
-			ctx.JSON(200, messages)
-		} else {
-			ctx.AbortWithError(404, errors.New("application does not exist"))
-		}
+		withPaging(ctx, func(params *pagingParams) {
+			if app := a.DB.GetApplicationByID(id); app != nil && app.UserID == auth.GetUserID(ctx) {
+				// the +1 is used to check if there are more messages and will be removed on buildWithPaging
+				messages := a.DB.GetMessagesByApplicationSince(id, params.Limit+1, params.Since)
+				ctx.JSON(200, buildWithPaging(ctx, params, messages))
+			} else {
+				ctx.AbortWithError(404, errors.New("application does not exist"))
+			}
+		})
 	})
 }
 
---
+++ model/paging.go
@@ -0,0 +1,54 @@
+package model
+
+// Paging Model
+//
+// The Paging holds holds information about the limit and making requests to the next page.
+//
+// swagger:model Paging
+type Paging struct {
+	// The request url for the next page. Empty/Null when no next page is available.
+	//
+	// read only: true
+	// required: false
+	// example: http://example.com/message?limit=50&since=123456
+	Next string `json:"next,omitempty"`
+	// The amount of messages that got returned in the current request.
+	//
+	// read only: true
+	// required: true
+	// example: 5
+	Size int `json:"size"`
+	// The ID of the last message returned in the current request. Use this as alternative to the next link.
+	//
+	// read only: true
+	// required: true
+	// example: 5
+	// min: 0
+	Since uint `json:"since"`
+	// The limit of the messages for the current request.
+	//
+	// read only: true
+	// required: true
+	// min: 1
+	// max: 200
+	// example: 123
+	Limit int `json:"limit"`
+}
+
+// PagedMessages Model
+//
+// Wrapper for the paging and the messages
+//
+// swagger:model PagedMessages
+type PagedMessages struct {
+	// The paging of the messages.
+	//
+	// read only: true
+	// required: true
+	Paging Paging `json:"paging"`
+	// The messages.
+	//
+	// read only: true
+	// required: true
+	Messages []*Message `json:"messages"`
+}
