fea8fa4999dda5f7ea627aa080d0e3d0c9a1fdec
Jannis Mattheis <contact@jmattheis.de>
2018-04-19 21:09:14 +0200

    Add missing timeago declaration

---
+++ ui/src/typedef/react-timeago.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-timeago' {
+    import React from "react";
+
+    export interface ITimeAgoProps {
+        date: string
+    }
+
+    export default class TimeAgo extends React.Component<ITimeAgoProps, any> {}
+}
+
