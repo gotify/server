cea8f751ce768a65e588638649a10d28ed38482d
Jannis Mattheis <contact@jmattheis.de>
2018-04-19 21:04:45 +0200

    Add api types

---
+++ ui/src/types.ts
@@ -0,0 +1,54 @@
+interface IApplication {
+    id: number
+    token: string
+    name: string
+    description: string
+    image: string
+}
+
+interface IClient {
+    id: number
+    token: string
+    name: string
+}
+
+interface IMessage {
+    id: number
+    appid: number
+    message: string
+    title: string
+    priority: number
+    date: string
+    image?: string
+}
+
+interface IPagedMessages {
+    paging: IPaging
+    messages: IMessage[]
+}
+
+interface IPaging {
+    next?: string
+    since?: number
+    size: number
+    limit: number
+}
+
+interface IUser {
+    id: number
+    name: string
+    admin: boolean
+}
+
+interface IVersion {
+    version: string
+    commit: string
+    buildDate: string
+}
+
+interface IAppMessages {
+    messages: IMessage[]
+    hasMore: boolean
+    nextSince: number,
+    id?: number
+}
