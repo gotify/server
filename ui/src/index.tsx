331bb607a711b50b8accda0b8144f8b3328021e8
Jannis Mattheis <contact@jmattheis.de>
2018-10-21 14:00:00 +0200

    Add WebSocketStore for handling the web socket

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import 'typeface-roboto-mono';
-import * as UserAction from './actions/UserAction';
+import './actions/defaultAxios';
 import * as config from './config';
 import Layout from './Layout';
 import registerServiceWorker from './registerServiceWorker';
 import * as Notifications from './stores/Notifications';
+import {currentUser} from './stores/CurrentUser';
+import AppStore from './stores/AppStore';
+import {reaction} from 'mobx';
+import {WebSocketStore} from './stores/WebSocketStore';
+import SnackManager from './stores/SnackManager';
 
 const defaultDevConfig = {
     url: 'http://localhost:80/',
@@ -35,7 +40,20 @@ declare global {
     } else {
         config.set(window.config || defaultDevConfig);
     }
-    UserAction.checkIfAlreadyLoggedIn();
+    const ws = new WebSocketStore(SnackManager.snack);
+    reaction(
+        () => currentUser.loggedIn,
+        (loggedIn) => {
+            if (loggedIn) {
+                ws.listen();
+            } else {
+                ws.close();
+            }
+            AppStore.refresh();
+        }
+    );
+
+    currentUser.tryAuthenticate();
     ReactDOM.render(<Layout />, document.getElementById('root'));
     registerServiceWorker();
 })();
