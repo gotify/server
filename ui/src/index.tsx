327b7deca6e904c2eba86b87d078ed5b6fd08dfb
Jannis Mattheis <contact@jmattheis.de>
2018-10-21 16:23:52 +0200

    Move and rename axios

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import 'typeface-roboto-mono';
-import {initAxios} from './actions/axios';
+import {initAxios} from './apiAuth';
 import * as config from './config';
 import Layout from './layout/Layout';
 import registerServiceWorker from './registerServiceWorker';
