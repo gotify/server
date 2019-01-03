36e0ab2a991288b2e5c8136e76936bcd01d37198
Jannis Mattheis <contact@jmattheis.de>
2019-01-03 15:52:12 +0100

    Correctly fill swagger host field

--- docs/swagger_test.go
+++ docs/swagger_test.go
@@ -2,6 +2,7 @@ package docs
 
 import (
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -13,11 +14,16 @@ func TestServe(t *testing.T) {
 	mode.Set(mode.TestDev)
 	recorder := httptest.NewRecorder()
 	ctx, _ := gin.CreateTestContext(recorder)
+	withURL(ctx, "http", "example.com")
+
 	ctx.Request = httptest.NewRequest("GET", "/swagger", nil)
-	ctx.Request.URL.Host = "localhost"
 
 	Serve(ctx)
 
 	content := recorder.Body.String()
 	assert.NotEmpty(t, content)
+}
+
+func withURL(ctx *gin.Context, scheme, host string) {
+	ctx.Set("location", &url.URL{Scheme: scheme, Host: host})
 }
