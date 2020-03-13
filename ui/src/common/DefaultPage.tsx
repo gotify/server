07b02264126d808da1dfac8f41edc451773da8c6
Yasa Akbulut <yasa@spotify.com>
2020-03-13 11:52:33 +0100

    Wrap control buttons on DefaultPage

--- ui/src/common/DefaultPage.tsx
+++ ui/src/common/DefaultPage.tsx
@@ -11,7 +11,7 @@ interface IProps {
 const DefaultPage: FC<IProps> = ({title, rightControl, maxWidth = 700, children}) => (
     <main style={{margin: '0 auto', maxWidth}}>
         <Grid container spacing={4}>
-            <Grid item xs={12} style={{display: 'flex'}}>
+            <Grid item xs={12} style={{display: 'flex', flexWrap: 'wrap'}}>
                 <Typography variant="h4" style={{flex: 1}}>
                     {title}
                 </Typography>
