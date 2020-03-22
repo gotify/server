simhaonline/server-1

Make layout responsive

ui/src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import {Theme, WithStyles, withStyles} from '@material-ui/core/styles';
+import {createStyles, Theme, WithStyles, withStyles} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -10,18 +10,45 @@ import DevicesOther from '@material-ui/icons/DevicesOther';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import Highlight from '@material-ui/icons/Highlight';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import MenuIcon from '@material-ui/icons/Menu';
 import Apps from '@material-ui/icons/Apps';
 import SupervisorAccount from '@material-ui/icons/SupervisorAccount';
 import React, {Component, CSSProperties} from 'react';
 import {Link} from 'react-router-dom';
 import {observer} from 'mobx-react';
+import {Hidden, PropTypes, withWidth} from "@material-ui/core";
+import {Breakpoint} from "@material-ui/core/styles/createBreakpoints";
 
-const styles = (theme: Theme) => ({
+const styles = (theme: Theme) => createStyles({
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
+        [theme.breakpoints.down('xs')]: {
+            paddingBottom: 10
+        },
+    },
+    toolbar: {
+        justifyContent: 'space-between',
+        [theme.breakpoints.down('xs')]: {
+            flexWrap: 'wrap'
+        },
+    },
+    menuButtons: {
+        display: 'flex',
+        [theme.breakpoints.down('sm')]: {
+            flex: 1,
+        },
+        justifyContent: 'center',
+        [theme.breakpoints.down('xs')]: {
+            flexBasis: '100%',
+            marginTop: 5,
+            order: 1,
+            justifyContent: 'space-between',
+        },
     },
     title: {
-        flex: 1,
+        [theme.breakpoints.up('md')]: {
+            flex: 1,
+        },
         display: 'flex',
         alignItems: 'center',
     },
@@ -34,7 +61,7 @@ const styles = (theme: Theme) => ({
     },
 });
 
-type Styles = WithStyles<'link' | 'titleName' | 'title' | 'appBar'>;
+type Styles = WithStyles<'link' | 'menuButtons' | 'toolbar' | 'titleName' | 'title' | 'appBar'>;
 
 interface IProps extends Styles {
     loggedIn: boolean;
@@ -45,16 +72,20 @@ interface IProps extends Styles {
     showSettings: VoidFunction;
     logout: VoidFunction;
     style: CSSProperties;
+    width: Breakpoint;
+    setNavOpen: (open: boolean) => void;
 }
 
 @observer
 class Header extends Component<IProps> {
     public render() {
-        const {classes, version, name, loggedIn, admin, toggleTheme, logout, style} = this.props;
+        const {classes, version, name, loggedIn, admin, toggleTheme, logout, style, setNavOpen, width} = this.props;
+
+        const position = width === 'xs' ? 'sticky' : 'fixed';
 
         return (
-            <AppBar position="absolute" style={style} className={classes.appBar}>
-                <Toolbar>
+            <AppBar position={position} style={style} className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
                     <div className={classes.title}>
                         <Link to="/" className={classes.link}>
                             <Typography variant="h5" className={classes.titleName} color="inherit">
@@ -69,65 +100,96 @@ class Header extends Component<IProps> {
                             </Typography>
                         </a>
                     </div>
-                    {loggedIn && this.renderButtons(name, admin, logout)}
-                    <IconButton onClick={toggleTheme} color="inherit">
-                        <Highlight />
-                    </IconButton>
-
-                    <a href="https://github.com/gotify/server" className={classes.link}>
-                        <IconButton color="inherit">
-                            <GitHubIcon />
+                    {loggedIn && this.renderButtons(name, admin, logout, width, setNavOpen)}
+                    <div>
+                        <IconButton onClick={toggleTheme} color="inherit">
+                            <Highlight />
                         </IconButton>
-                    </a>
+
+                        <a href="https://github.com/gotify/server" className={classes.link}>
+                            <IconButton color="inherit">
+                                <GitHubIcon />
+                            </IconButton>
+                        </a>
+                    </div>
                 </Toolbar>
             </AppBar>
         );
     }
 
-    private renderButtons(name: string, admin: boolean, logout: VoidFunction) {
+    private renderButtons(name: string, admin: boolean, logout: VoidFunction, width: Breakpoint, setNavOpen: (open: boolean) => void) {
         const {classes, showSettings} = this.props;
         return (
-            <div>
-                {admin ? (
-                    <Link className={classes.link} to="/users" id="navigate-users">
-                        <Button color="inherit">
-                            <SupervisorAccount />
-                            &nbsp;users
-                        </Button>
-                    </Link>
-                ) : (
-                    ''
-                )}
+            <div className={classes.menuButtons}>
+                <Hidden smUp implementation="css">
+                    <ResponsiveButton
+                        icon={<MenuIcon/>}
+                        onClick={() => setNavOpen(true)}
+                        label="menu"
+                        width={width}
+                        color="inherit" />
+                </Hidden>
+                {
+                    admin && (
+                        <Link className={classes.link} to="/users" id="navigate-users">
+                            <ResponsiveButton
+                                icon={<SupervisorAccount/>}
+                                label="users"
+                                width={width}
+                                color="inherit"/>
+                        </Link>
+                    )
+                }
                 <Link className={classes.link} to="/applications" id="navigate-apps">
-                    <Button color="inherit">
-                        <Chat />
-                        &nbsp;apps
-                    </Button>
+                    <ResponsiveButton
+                        icon={<Chat/>}
+                        label="apps"
+                        width={width}
+                        color="inherit"/>
                 </Link>
                 <Link className={classes.link} to="/clients" id="navigate-clients">
-                    <Button color="inherit">
-                        <DevicesOther />
-                        &nbsp;clients
-                    </Button>
+                    <ResponsiveButton
+                        icon={<DevicesOther/>}
+                        label="clients"
+                        width={width}
+                        color="inherit"/>
                 </Link>
                 <Link className={classes.link} to="/plugins" id="navigate-plugins">
-                    <Button color="inherit">
-                        <Apps />
-                        &nbsp;plugins
-                    </Button>
+                    <ResponsiveButton
+                        icon={<Apps/>}
+                        label="plugins"
+                        width={width}
+                        color="inherit"/>
                 </Link>
-                <Button color="inherit" onClick={showSettings} id="changepw">
-                    <AccountCircle />
-                    &nbsp;
-                    {name}
-                </Button>
-                <Button color="inherit" onClick={logout} id="logout">
-                    <ExitToApp />
-                    &nbsp;Logout
-                </Button>
+                <ResponsiveButton
+                    icon={<AccountCircle/>}
+                    label={name}
+                    onClick={showSettings}
+                    id="changepw"
+                    width={width}
+                    color="inherit"/>
+                <ResponsiveButton
+                    icon={<ExitToApp/>}
+                    label="Logout"
+                    onClick={logout}
+                    id="logout"
+                    width={width}
+                    color="inherit"/>
             </div>
         );
     }
 }
 
-export default withStyles(styles, {withTheme: true})(Header);
+const ResponsiveButton: React.FC<{ width: Breakpoint; color: PropTypes.Color, label: string, id?: string, onClick?: () => void, icon: React.ReactNode }> = ({width, icon, children, label, ...rest}) => {
+    if (width === 'xs' || width === 'sm') {
+        return (<IconButton {...rest}>{icon}</IconButton>);
+    }
+    return (
+        <Button startIcon={icon} {...rest}>
+            {label}
+        </Button>
+    );
+};
+
+
+export default withWidth()(withStyles(styles, {withTheme: true})(Header));

gotify/server

ui/src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import {Theme, WithStyles, withStyles} from '@material-ui/core/styles';
+import {createStyles, Theme, WithStyles, withStyles} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -10,31 +10,59 @@ import DevicesOther from '@material-ui/icons/DevicesOther';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import Highlight from '@material-ui/icons/Highlight';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import MenuIcon from '@material-ui/icons/Menu';
 import Apps from '@material-ui/icons/Apps';
 import SupervisorAccount from '@material-ui/icons/SupervisorAccount';
 import React, {Component, CSSProperties} from 'react';
 import {Link} from 'react-router-dom';
 import {observer} from 'mobx-react';
+import {Hidden, PropTypes, withWidth} from '@material-ui/core';
+import {Breakpoint} from '@material-ui/core/styles/createBreakpoints';
 
-const styles = (theme: Theme) => ({
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-    },
-    title: {
-        flex: 1,
-        display: 'flex',
-        alignItems: 'center',
-    },
-    titleName: {
-        paddingRight: 10,
-    },
-    link: {
-        color: 'inherit',
-        textDecoration: 'none',
-    },
-});
+const styles = (theme: Theme) =>
+    createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            [theme.breakpoints.down('xs')]: {
+                paddingBottom: 10,
+            },
+        },
+        toolbar: {
+            justifyContent: 'space-between',
+            [theme.breakpoints.down('xs')]: {
+                flexWrap: 'wrap',
+            },
+        },
+        menuButtons: {
+            display: 'flex',
+            [theme.breakpoints.down('sm')]: {
+                flex: 1,
+            },
+            justifyContent: 'center',
+            [theme.breakpoints.down('xs')]: {
+                flexBasis: '100%',
+                marginTop: 5,
+                order: 1,
+                justifyContent: 'space-between',
+            },
+        },
+        title: {
+            [theme.breakpoints.up('md')]: {
+                flex: 1,
+            },
+            display: 'flex',
+            alignItems: 'center',
+        },
+        titleName: {
+            paddingRight: 10,
+        },
+        link: {
+            color: 'inherit',
+            textDecoration: 'none',
+        },
+    });
 
-type Styles = WithStyles<'link' | 'titleName' | 'title' | 'appBar'>;
+type Styles = WithStyles<'link' | 'menuButtons' | 'toolbar' | 'titleName' | 'title' | 'appBar'>;
 
 interface IProps extends Styles {
     loggedIn: boolean;
@@ -45,16 +73,31 @@ interface IProps extends Styles {
     showSettings: VoidFunction;
     logout: VoidFunction;
     style: CSSProperties;
+    width: Breakpoint;
+    setNavOpen: (open: boolean) => void;
 }
 
 @observer
 class Header extends Component<IProps> {
     public render() {
-        const {classes, version, name, loggedIn, admin, toggleTheme, logout, style} = this.props;
+        const {
+            classes,
+            version,
+            name,
+            loggedIn,
+            admin,
+            toggleTheme,
+            logout,
+            style,
+            setNavOpen,
+            width,
+        } = this.props;
+
+        const position = width === 'xs' ? 'sticky' : 'fixed';
 
         return (
-            <AppBar position="absolute" style={style} className={classes.appBar}>
-                <Toolbar>
+            <AppBar position={position} style={style} className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
                     <div className={classes.title}>
                         <Link to="/" className={classes.link}>
                             <Typography variant="h5" className={classes.titleName} color="inherit">
@@ -69,65 +112,108 @@ class Header extends Component<IProps> {
                             </Typography>
                         </a>
                     </div>
-                    {loggedIn && this.renderButtons(name, admin, logout)}
-                    <IconButton onClick={toggleTheme} color="inherit">
-                        <Highlight />
-                    </IconButton>
-
-                    <a href="https://github.com/gotify/server" className={classes.link}>
-                        <IconButton color="inherit">
-                            <GitHubIcon />
+                    {loggedIn && this.renderButtons(name, admin, logout, width, setNavOpen)}
+                    <div>
+                        <IconButton onClick={toggleTheme} color="inherit">
+                            <Highlight />
                         </IconButton>
-                    </a>
+
+                        <a href="https://github.com/gotify/server" className={classes.link}>
+                            <IconButton color="inherit">
+                                <GitHubIcon />
+                            </IconButton>
+                        </a>
+                    </div>
                 </Toolbar>
             </AppBar>
         );
     }
 
-    private renderButtons(name: string, admin: boolean, logout: VoidFunction) {
+    private renderButtons(
+        name: string,
+        admin: boolean,
+        logout: VoidFunction,
+        width: Breakpoint,
+        setNavOpen: (open: boolean) => void
+    ) {
         const {classes, showSettings} = this.props;
         return (
-            <div>
-                {admin ? (
+            <div className={classes.menuButtons}>
+                <Hidden smUp implementation="css">
+                    <ResponsiveButton
+                        icon={<MenuIcon />}
+                        onClick={() => setNavOpen(true)}
+                        label="menu"
+                        width={width}
+                        color="inherit"
+                    />
+                </Hidden>
+                {admin && (
                     <Link className={classes.link} to="/users" id="navigate-users">
-                        <Button color="inherit">
-                            <SupervisorAccount />
-                            &nbsp;users
-                        </Button>
+                        <ResponsiveButton
+                            icon={<SupervisorAccount />}
+                            label="users"
+                            width={width}
+                            color="inherit"
+                        />
                     </Link>
-                ) : (
-                    ''
                 )}
                 <Link className={classes.link} to="/applications" id="navigate-apps">
-                    <Button color="inherit">
-                        <Chat />
-                        &nbsp;apps
-                    </Button>
+                    <ResponsiveButton icon={<Chat />} label="apps" width={width} color="inherit" />
                 </Link>
                 <Link className={classes.link} to="/clients" id="navigate-clients">
-                    <Button color="inherit">
-                        <DevicesOther />
-                        &nbsp;clients
-                    </Button>
+                    <ResponsiveButton
+                        icon={<DevicesOther />}
+                        label="clients"
+                        width={width}
+                        color="inherit"
+                    />
                 </Link>
                 <Link className={classes.link} to="/plugins" id="navigate-plugins">
-                    <Button color="inherit">
-                        <Apps />
-                        &nbsp;plugins
-                    </Button>
+                    <ResponsiveButton
+                        icon={<Apps />}
+                        label="plugins"
+                        width={width}
+                        color="inherit"
+                    />
                 </Link>
-                <Button color="inherit" onClick={showSettings} id="changepw">
-                    <AccountCircle />
-                    &nbsp;
-                    {name}
-                </Button>
-                <Button color="inherit" onClick={logout} id="logout">
-                    <ExitToApp />
-                    &nbsp;Logout
-                </Button>
+                <ResponsiveButton
+                    icon={<AccountCircle />}
+                    label={name}
+                    onClick={showSettings}
+                    id="changepw"
+                    width={width}
+                    color="inherit"
+                />
+                <ResponsiveButton
+                    icon={<ExitToApp />}
+                    label="Logout"
+                    onClick={logout}
+                    id="logout"
+                    width={width}
+                    color="inherit"
+                />
             </div>
         );
     }
 }
 
-export default withStyles(styles, {withTheme: true})(Header);
+const ResponsiveButton: React.FC<{
+    width: Breakpoint;
+    color: PropTypes.Color;
+    label: string;
+    id?: string;
+    onClick?: () => void;
+    icon: React.ReactNode;
+}> = ({width, icon, children, label, ...rest}) => {
+    if (width === 'xs' || width === 'sm') {
+        return <IconButton {...rest}>{icon}</IconButton>;
+    }
+    return (
+        <Button startIcon={icon} {...rest}>
+            {label}
+        </Button>
+    );
+};
+
+export default withWidth()(withStyles(styles, {withTheme: true})(Header));