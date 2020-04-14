1b1629efb9926e48f64b49e3e5e13d41ceedf144
Stewart Thomson <stewartthomson3@gmail.com>
2020-04-13 22:17:55 -0400

    - fixed format

--- ui/src/layout/Header.tsx
+++ ui/src/layout/Header.tsx
@@ -118,7 +118,11 @@ class Header extends Component<IProps> {
                             <Highlight />
                         </IconButton>
 
-                        <a href="https://github.com/gotify/server" className={classes.link} target="_blank" rel="noopener noreferrer">
+                        <a
+                            href="https://github.com/gotify/server"
+                            className={classes.link}
+                            target="_blank"
+                            rel="noopener noreferrer">
                             <IconButton color="inherit">
                                 <GitHubIcon />
                             </IconButton>
