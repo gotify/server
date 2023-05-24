a37afcebfc0d012f2ff49a9281208248d82e389a
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2023-05-24 12:25:51 -0400

    Improve log format (#567)

--- router/router.go
+++ router/router.go
@@ -191,8 +191,8 @@ func logFormatter(param gin.LogFormatterParams) string {
 		param.Latency = param.Latency - param.Latency%time.Second
 	}
 	path := tokenRegexp.ReplaceAllString(param.Path, "token=[masked]")
-	return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
-		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+	return fmt.Sprintf("%v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+		param.TimeStamp.Format(time.RFC3339),
 		statusColor, param.StatusCode, resetColor,
 		param.Latency,
 		param.ClientIP,
