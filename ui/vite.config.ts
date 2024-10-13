289f1002acff1c26fd2385ab0c12c737cda1604f
Matthias Fechner <matthias@fechner.net>
2024-10-13 15:05:23 +0300

    Use plugin-react instead of plugin-react-swc, as that will not work on FreeBSD

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,43 +1,47 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "")
-
-    return {
-        build: {
-            outDir: 'build',
-            emptyOutDir: true,
-            sourcemap: true,
-        },
-        plugins: [ react({ tsDecorators: true }) ],
-        define: {
-            // Some libraries use the global object, even though it doesn't exist in the browser.
-            // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
-            // https://github.com/vitejs/vite/discussions/5912
-            global: {},
-        },
-        server: {
-            proxy: {
-                "/api": {
-                    target: 'http://localhost:3000/',
-                    changeOrigin: true,
-                    secure: false,
-                    rewrite: (p) => p.replace(/^\/api/, ""),
-                },
+export default defineConfig({
+    build: {
+        outDir: 'build',
+        emptyOutDir: true,
+        sourcemap: true,
+    },
+    plugins: [
+        react({
+            babel: {
+                parserOpts: {
+                    plugins: [ 'decorators-legacy' ],
+                }
+            }
+        })
+    ],
+    define: {
+        // Some libraries use the global object, even though it doesn't exist in the browser.
+        // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
+        // https://github.com/vitejs/vite/discussions/5912
+        global: {},
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000/',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (p) => p.replace(/^\/api/, ''),
             },
-            cors: false,
         },
-        preview: {
-            proxy: {
-                "/api": {
-                    target: 'http://localhost:3000/',
-                    changeOrigin: true,
-                    secure: false,
-                    rewrite: (p) => p.replace(/^\/api/, ""),
-                },
+        cors: false,
+    },
+    preview: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000/',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (p) => p.replace(/^\/api/, ''),
             },
-            cors: false,
         },
-    };
+        cors: false,
+    },
 });
