560e3d948729db0649e0c62524d6f20fe49f3b65
Matthias Fechner <matthias@fechner.net>
2024-10-16 09:22:55 +0300

    Define global as it is required by react-codemirror2

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import "./init";
+
 import 'typeface-roboto';
 import {initAxios} from './apiAuth';
 import * as config from './config';
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
         // Some libraries use the global object, even though it doesn't exist in the browser.
         // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
         // https://github.com/vitejs/vite/discussions/5912
-        global: {},
+        //global: {},
     },
     server: {
         proxy: {
