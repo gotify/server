23e5fdee14cf9ccf8696c33cb08776634836f40e
Matthias Fechner <matthias@fechner.net>
2025-01-06 16:17:40 +0200

    Do not inline sourcemaps anymore to reduce the file size for production bundle

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         outDir: 'build',
         assetsDir: 'static',
         emptyOutDir: true,
-        sourcemap: 'inline',
+        sourcemap: true,
         rollupOptions: {
             plugins: [
                 {
