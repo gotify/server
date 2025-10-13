5244fa18fc880d2442517c0cb980c936eb40f65c
饺子w (Yumechi) <35571479+eternal-flame-AD@users.noreply.github.com>
2025-10-13 02:11:55 +0000

    Format code

--- ui/src/common/Markdown.tsx
+++ ui/src/common/Markdown.tsx
@@ -6,7 +6,11 @@ import gfm from 'remark-gfm';
 // Copy from mlflow/server/js/src/shared/web-shared/genai-markdown-renderer/GenAIMarkdownRenderer.tsx
 // Related PR: https://github.com/mlflow/mlflow/pull/16761
 const urlTransform: UrlTransform = (value) => {
-    if (value.startsWith('data:image/png;') || value.startsWith('data:image/jpeg;') || value.startsWith('data:image/gif;')) {
+    if (
+        value.startsWith('data:image/png;') ||
+        value.startsWith('data:image/jpeg;') ||
+        value.startsWith('data:image/gif;')
+    ) {
         return value;
     }
     return defaultUrlTransform(value);
