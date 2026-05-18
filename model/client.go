Smartesting/gotify

feat: clean up expired devices

model/client.go
@@ -41,4 +41,30 @@ type Client struct {
 	//
 	// read only: true
 	ElevatedUntil *time.Time `json:"elevatedUntil,omitempty"`
+	// The number of seconds of inactivity after which the client is removed.
+	// 0 means the client never expires.
+	//
+	// example: 2592000
+	ExpiresAfterInactivitySeconds uint `gorm:"default:0;not null" form:"expiresAfterInactivitySeconds" query:"expiresAfterInactivitySeconds" json:"expiresAfterInactivitySeconds"`
+	// The time at which this client will expire due to inactivity, or null if it never expires.
+	//
+	// read only: true
+	// example: 2019-01-01T00:00:00Z
+	ExpiresAt *time.Time `gorm:"-" json:"expiresAt,omitempty"`
+}
+
+func (c *Client) PopulateExpiresAt() {
+	c.ExpiresAt = c.GetExpiresAt()
+}
+
+func (c *Client) GetExpiresAt() *time.Time {
+	if c.ExpiresAfterInactivitySeconds == 0 {
+		return nil
+	}
+	reference := c.CreatedAt
+	if c.LastUsed != nil {
+		reference = *c.LastUsed
+	}
+	expiry := reference.Add(time.Duration(c.ExpiresAfterInactivitySeconds) * time.Second)
+	return &expiry
 }

router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"path/filepath"
 	"regexp"
@@ -71,7 +72,16 @@ func Create(db *database.GormDatabase, vInfo *model.VersionInfo, conf *config.Co
 		for range ticker.C {
 			connectedTokens := streamHandler.CollectConnectedClientTokens()
 			now := time.Now()
-			db.UpdateClientTokensLastUsed(connectedTokens, &now)
+			if err := db.UpdateClientTokensLastUsed(connectedTokens, &now); err != nil {
+				log.Println("Error updating last used", err)
+			}
+			if expired, err := db.CleanupExpiredClients(now); err == nil {
+				for _, c := range expired {
+					streamHandler.NotifyDeletedClient(c.UserID, c.Token)
+				}
+			} else {
+				log.Println("Error cleaning up expired clients", err)
+			}
 		}
 	}()
 	authentication := auth.Auth{DB: db, SecureCookie: conf.Server.SecureCookie}

gotify/server

model/client.go
@@ -41,4 +41,30 @@ type Client struct {
 	//
 	// read only: true
 	ElevatedUntil *time.Time `json:"elevatedUntil,omitempty"`
+	// The number of seconds of inactivity after which the client is removed.
+	// 0 means the client never expires.
+	//
+	// example: 2592000
+	ExpiresAfterInactivitySeconds uint `gorm:"default:0;not null" form:"expiresAfterInactivitySeconds" query:"expiresAfterInactivitySeconds" json:"expiresAfterInactivitySeconds"`
+	// The time at which this client will expire due to inactivity, or null if it never expires.
+	//
+	// read only: true
+	// example: 2019-01-01T00:00:00Z
+	ExpiresAt *time.Time `gorm:"index" json:"expiresAt,omitempty"`
+}
+
+func (c *Client) PopulateExpiresAt() {
+	c.ExpiresAt = c.calculateExpiresAt()
+}
+
+func (c *Client) calculateExpiresAt() *time.Time {
+	if c.ExpiresAfterInactivitySeconds == 0 {
+		return nil
+	}
+	reference := c.CreatedAt
+	if c.LastUsed != nil {
+		reference = *c.LastUsed
+	}
+	expiry := reference.Add(time.Duration(c.ExpiresAfterInactivitySeconds) * time.Second)
+	return &expiry
 }

router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"path/filepath"
 	"regexp"
@@ -71,7 +72,16 @@ func Create(db *database.GormDatabase, vInfo *model.VersionInfo, conf *config.Co
 		for range ticker.C {
 			connectedTokens := streamHandler.CollectConnectedClientTokens()
 			now := time.Now()
-			db.UpdateClientTokensLastUsed(connectedTokens, &now)
+			if err := db.UpdateClientTokensLastUsedAndExpiresAt(connectedTokens, &now); err != nil {
+				log.Println("Error updating last used", err)
+			}
+			if expired, err := db.CleanupExpiredClients(now); err == nil {
+				for _, c := range expired {
+					streamHandler.NotifyDeletedClient(c.UserID, c.Token)
+				}
+			} else {
+				log.Println("Error cleaning up expired clients", err)
+			}
 		}
 	}()
 	authentication := auth.Auth{DB: db, SecureCookie: conf.Server.SecureCookie}